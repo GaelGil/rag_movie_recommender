cd392f703d5724567ccb2918cdb60cba5ec940a6
Gael <gaelgilbello27@gmail.com>
2025-10-03 16:26:54 -0700

    updating alembic settings

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -4,6 +4,9 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from app.core.config import settings
 from alembic import context
+from app.core.db import Base
+from app.models.User import User
+# from app.models.Chat import ChatMessage, ToolHistory, ChatSession, Movie
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
