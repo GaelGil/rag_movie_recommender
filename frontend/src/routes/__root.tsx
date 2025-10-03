3908bab2b23adb92bc91de2cb2c45b395659e8f3
Gael <gaelgilbello27@gmail.com>
2025-10-02 18:13:05 -0700

    removing old stuff

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,15 +1,11 @@
-import * as React from 'react'
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
-export const Route = createRootRoute({
-  component: RootComponent,
-})
+const RootLayout = () => (
+  <>
+    <Outlet />
+    <TanStackRouterDevtools />
+  </>
+);
 
-function RootComponent() {
-  return (
-    <React.Fragment>
-      <div>Hello "__root"!</div>
-      <Outlet />
-    </React.Fragment>
-  )
-}
+export const Route = createRootRoute({ component: RootLayout });
