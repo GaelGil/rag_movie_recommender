25e4b9be14f913f5aa5f282e2560bd945e14e981
Gael <gaelgilbello27@gmail.com>
2025-10-03 16:10:11 -0700

    updating stuff

--- backend/app/api/routes/user.py
+++ backend/app/api/routes/user.py
@@ -1,13 +1,11 @@
 from fastapi import APIRouter, Depends
-
-# from sqlalchemy.orm import Session
-from app.core.db import get_db
+from app.core.deps import get_current_user
+from app.schemas.User import ReadUser
 from app.models import User
-
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 
-@router.get("/me")
-async def read_users_me(current_user: User = Depends(get_db)):
+@router.get("/me", response_model=ReadUser)
+def read_me(current_user: User = Depends(get_current_user)):
     return current_user
