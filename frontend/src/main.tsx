2d420b0d6b5e33204a658e53d24c27f76fc64f47
Gael <gaelgilbello27@gmail.com>
2025-10-02 18:09:38 -0700

    updating to tanstack

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,27 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import "./css/index.css";
-import App from "./App.tsx";
-import { UserProvider } from "./context/UserContext.tsx";
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
-  </StrictMode>
-);
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// Render the app
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    ,
+    react(),
+    tailwindcss(),
+  ],
   server: {
     host: true, // equivalent to 0.0.0.0
     port: 5173,
