8bd2c91ba6d2250c15cd6151c301d89bab3bf419
Gael Gil <gaelgilbello27@gmail.com>
2026-01-14 13:54:33 -0800

    removing more stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,11 @@
-import {
-  MutationCache,
-  QueryCache,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { ApiError, OpenAPI } from "./client";
 import { CustomProvider } from "./components/ui/provider";
 import { routeTree } from "./routeTree.gen";
 import "./main.css";
 
-OpenAPI.BASE = import.meta.env.VITE_API_URL || "http://localhost:8000";
-OpenAPI.TOKEN = async () => {
-  return localStorage.getItem("access_token") || "";
-};
-
-const handleApiError = (error: Error) => {
-  if (error instanceof ApiError && [401, 403].includes(error.status)) {
-    localStorage.removeItem("access_token");
-    window.location.href = "/login";
-  }
-};
-const queryClient = new QueryClient({
-  queryCache: new QueryCache({
-    onError: handleApiError,
-  }),
-  mutationCache: new MutationCache({
-    onError: handleApiError,
-  }),
-});
+// const BACKEND_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 const router = createRouter({ routeTree });
 declare module "@tanstack/react-router" {
@@ -42,9 +17,7 @@ declare module "@tanstack/react-router" {
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <CustomProvider>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
+      <RouterProvider router={router} />
     </CustomProvider>
   </StrictMode>
 );
