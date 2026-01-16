33680f402b6162dd63f11b7ca82df81ffc7a125b
Gael Gil <gaelgilbello27@gmail.com>
2026-01-16 00:34:11 -0800

    added simple graph

---
+++ src/components/myGrid.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useRef } from "react";
+import Plotly from "plotly.js-dist-min";
+import * as d3 from "d3";
+
+export const Scatter3DPlot: React.FC = () => {
+  const plotRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // load CSV data
+    d3.csv(
+      "https://raw.githubusercontent.com/plotly/datasets/master/alpha_shape.csv"
+    )
+      .then((rows) => {
+        const unpack = (rows: any[], key: string) =>
+          rows.map((r) => Number(r[key]));
+
+        const data: Plotly.Data[] = [
+          {
+            x: unpack(rows, "x"),
+            y: unpack(rows, "y"),
+            z: unpack(rows, "z"),
+            mode: "markers",
+            type: "scatter3d",
+            marker: { color: "rgb(23,190,207)", size: 2 },
+          },
+          {
+            type: "mesh3d",
+            opacity: 0.1,
+            x: unpack(rows, "x"),
+            y: unpack(rows, "y"),
+            z: unpack(rows, "z"),
+          },
+        ];
+
+        const layout: Partial<Plotly.Layout> = {
+          autosize: true,
+          height: 480,
+          scene: {
+            aspectratio: { x: 1, y: 1, z: 1 },
+            camera: {
+              center: { x: 0, y: 0, z: 0 },
+              eye: { x: 1.25, y: 1.25, z: 1.25 },
+              up: { x: 0, y: 0, z: 1 },
+            },
+            xaxis: { type: "linear", zeroline: false },
+            yaxis: { type: "linear", zeroline: false },
+            zaxis: { type: "linear", zeroline: false },
+          },
+          title: { text: "3D Point Clustering" },
+          margin: { l: 0, r: 0, b: 0, t: 0, pad: 4 },
+          showlegend: false,
+          paper_bgcolor: "rgb(0,0,0)",
+          plot_bgcolor: "rgb(0,0,0)",
+        };
+
+        // render plot
+        if (plotRef.current) {
+          Plotly.newPlot(plotRef.current, data, layout);
+        }
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  return <div ref={plotRef} style={{ width: "100%", height: "480px" }} />;
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { Container, AppShell, Group } from "@mantine/core";
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { useState } from "react";
-
+import { Scatter3DPlot } from "@/components/myGrid";
 import Grid from "@/components/Common/Grid";
 export const Route = createFileRoute("/")({
   component: Graph3D,
@@ -32,12 +32,13 @@ function Graph3D() {
         </Container>
       </AppShell.Header>
       <AppShell.Main>
-        <Grid
+        {/* <Grid
           hoveredId={hoveredId}
           isOpen={isOpen}
           setIsOpen={setIsOpen}
           handleClick={handleClick}
-        />
+        /> */}
+        <Scatter3DPlot />
       </AppShell.Main>
     </AppShell>
   );
